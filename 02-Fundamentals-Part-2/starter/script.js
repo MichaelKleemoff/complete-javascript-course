e87fe61ff78f1ed1d0a13003f05a509d91f2db00
Michael Kleemoff <michaelkleemoff2@gmail.com>
2025-03-03 09:56:52 -0500

    Complete '49. The While Loop'.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -355,7 +355,7 @@ for (let i = 0; i < jonasArray.length; i++) {
 
 	console.log(jonasArray[i], typeof jonasArray[i]);
 }
-*/
+
 // 48. LOOPING BACKWARDS AND LOOPS IN LOOPS -
 
 const jonasArray = [
@@ -379,4 +379,25 @@ for (let exercise = 1; exercise < 4; exercise++) {
 	for (let rep = 1; rep < 6; rep++) {
 		console.log(`Exercise ${exercise}: Lifting weights repetition ${rep} 🏋️‍♂️`);
 	}
+}
+*/
+
+// 49. THE WHILE LOOP -
+
+// for (let rep = 1; rep <= 10; rep++) {
+// 	console.log(`Lifting weights repetition ${rep} 🏋️‍♂️`);
+// }
+
+// let rep = 1;
+// while (rep <= 10) {
+// 	console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♂️`);
+// 	rep++;
+// }
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+	console.log(`You rolled a ${dice}`);
+	dice = Math.trunc(Math.random() * 6) + 1;
+	if (dice === 6) console.log('Loop is about to end...');
 }
